c8ac6159dea88953d25ef0fb239bc2f5ef8d44fc
SaudMaashi <saudm45q@gmail.com>
2024-09-19 21:05:21 +0300

    Restarting App

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:movies_app/screens/onboarding/onboarding_screen.dart';
-import 'package:movies_app/utilities/constants.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Future.delayed(const Duration(seconds: 3));
   FlutterNativeSplash.remove();
-  runApp(const MoviesApp());
+  runApp(const NewsApp());
 }
 
-class MoviesApp extends StatelessWidget {
-  const MoviesApp({super.key});
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const OnboardingScreen(),
-      theme: ThemeData(
-          scaffoldBackgroundColor: const Color(0xFF303030),
-          primarySwatch: kPrimaryColor,
-          fontFamily: GoogleFonts.inriaSansTextTheme().bodyMedium!.fontFamily,
-          brightness: Brightness.dark,
-          useMaterial3: false),
-    );
+    return const Placeholder();
   }
 }
